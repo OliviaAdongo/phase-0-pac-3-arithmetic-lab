cd4fe9d6b79658a16674ee7c92c9e6235cc473ee
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-05 23:44:40 +0300

    redo

--- test/index-test.js
+++ test/index-test.js
@@ -4,7 +4,7 @@ beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
 })
-
+ 
 describe('basic math functions', function () {
   it("'add()' is a valid function", function() {
     expect(add).toExist
