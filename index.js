f6fe8050d005ecbaeaad1dc4586fdbc2bdca862a
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-06 08:28:48 +0300

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ return a * b;
 function divide(a, b){
 return a/b;
 }
+function increment(n){
+    n++
+    return n;
+}
+function decrement(n){
+  n--
+  return n;
+}
+function makeInt(n){
+    return parseInt(n, 10);
+    
+}
+function preserveDecimal(n){
+    return parseFloat(n, 2.222)
+   
+}
